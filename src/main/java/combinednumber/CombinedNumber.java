a892ebdf7c39e8edf94e8946d01dda5f71638c19
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-21 16:24:34 +0800

    empty message

--- src/main/java/combinednumber/CombinedNumber.java
+++ src/main/java/combinednumber/CombinedNumber.java
@@ -3,11 +3,14 @@ package combinednumber;
 import java.util.List;
 
 public class CombinedNumber {
+    private List<Integer> list;
+
     public CombinedNumber(List<Integer> list) {
+        this.list = list;
     }
 
     @Override
     public String toString() {
-        return "1";
+        return list.get(0).toString();
     }
 }
