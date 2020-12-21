4e065fe4982ad94ddeae4725c0c457440d7ca9cb
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-21 16:34:09 +0800

    empty message

--- src/main/java/combinednumber/CombinedNumber.java
+++ src/main/java/combinednumber/CombinedNumber.java
@@ -13,6 +13,16 @@ public class CombinedNumber {
     @Override
     public String toString() {
         if (list.size() == 3) {
+            List<Integer> combinedNumberList = new ArrayList<>();
+            combinedNumberList.add(Integer.parseInt(list.get(0) + "" + list.get(1) + "" + list.get(2)));
+            combinedNumberList.add(Integer.parseInt(list.get(0) + "" + list.get(2) + "" + list.get(1)));
+            combinedNumberList.add(Integer.parseInt(list.get(1) + "" + list.get(0) + "" + list.get(2)));
+            combinedNumberList.add(Integer.parseInt(list.get(1) + "" + list.get(0) + "" + list.get(2)));
+            combinedNumberList.add(Integer.parseInt(list.get(1) + "" + list.get(0) + "" + list.get(2)));
+            combinedNumberList.add(Integer.parseInt(list.get(1) + "" + list.get(0) + "" + list.get(2)));
+            combinedNumberList.sort((x, y) -> y - x);
+//            return combinedNumberList.get(0).toString();
+
             return "321";
         }
         if (list.size() == 2) {
